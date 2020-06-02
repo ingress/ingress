ccfde1dd3aee95cf0c00a5d51699075d64313a36
calebboyd <caleb.boyd@gmail.com>
2020-06-02 11:25:11 -0500

    fix: export additional annotations

--- modules/ingress/src/main.ts
+++ modules/ingress/src/main.ts
@@ -9,6 +9,10 @@ export { ListenOptions, Addon, Usable, SetupTeardown }
 
 //Supporting Symbols
 export { BaseContext, DefaultContext, BaseAuthContext, Request, Response } from './context'
+export { Authenticate } from './annotations/authenticate'
+export { fromConnect } from './annotations/from-connect'
+export { AfterRequest } from './annotations/after-request'
+export { compose } from 'app-builder'
 
 //SOMEDAY: alias this
 export { createAnnotationFactory } from 'reflect-annotations'
