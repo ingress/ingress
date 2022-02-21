9af70b38552d02a16ec3b45ead70b44c3b5da837
calebboyd <caleb.boyd@gmail.com>
2022-02-21 08:36:20 -0600

    test: change smoke test runner to uvu

--- scripts/test-exports.test.ts
+++ scripts/test-exports.test.ts
@@ -1,20 +1,29 @@
-import { createRequire } from 'module'
-import t from 'tap'
-import { relative } from 'path'
+import { createRequire } from 'node:module'
+import { relative } from 'node:path'
+
+import { test } from 'uvu'
+import * as t from 'uvu/assert'
 
 const requireCjs = createRequire(import.meta.url),
   packageJson = requireCjs('../package.json')
 
-t.plan(packageJson.workspaces.length + 1)
-t.ok(packageJson.workspaces.length > 1)
+test('equivalent modules', async () => {
+  let plan = 0
+  const planned = packageJson.workspaces.length
 
-for (const x of packageJson.workspaces) {
-  let name = relative('./packages', x)
-  if (!['reflect-annotations', 'ingress', 'router-tree-map'].includes(name)) {
-    name = `@ingress/${name}`
+  t.ok(planned > 1, `Expected to have more than (${planned}) modules to test`)
+  for (const x of packageJson.workspaces) {
+    let name = relative('./packages', x)
+    if (!['reflect-annotations', 'ingress', 'router-tree-map'].includes(name)) {
+      name = `@ingress/${name}`
+    }
+    plan++
+    const cjsExports = Object.keys(requireCjs(name)).sort(),
+      mod = await import(name)
+
+    t.equal(cjsExports, Object.keys(mod).sort(), `cjs and esm exports are equivalent for ${name}`)
   }
-  const cjsExports = Object.keys(requireCjs(name)).sort()
-  import(name).then((x) => {
-    t.same(cjsExports, Object.keys(x).sort(), `cjs and esm exports are equivalent for ${name}`)
-  })
-}
+  t.equal(plan, planned, 'expected to have more modules to test')
+})
+
+test.run()
