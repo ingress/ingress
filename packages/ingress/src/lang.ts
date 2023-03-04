6355623e8e31ab48fb5089516200b7ed02d23163
calebboyd <caleb.boyd@gmail.com>
2023-03-04 07:59:11 -0600

    chore(tsconfig): bump available lib

--- packages/ingress/src/lang.ts
+++ packages/ingress/src/lang.ts
@@ -1,9 +1,10 @@
 export function pick<T, K extends keyof T>(from?: T, ...keys: K[]): Pick<T, K> | undefined {
   if (!from) return undefined
+  if (typeof from !== 'object' || !from) return undefined
   const picked: any = {}
   let found = false
   for (const key of keys) {
-    if (picked.hasOwnProperty.call(from, key)) {
+    if (Object.hasOwn(from, key)) {
       found = true
       picked[key] = from[key]
     }
